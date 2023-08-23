145b61f4b748f525234486149a14c43e26368c82
JuancarlosMuller <118220005+JuancarlosMuller@users.noreply.github.com>
2023-08-23 17:50:04 +0000

    cuarto

--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -5,10 +5,11 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
-    const { charactersData, setCharactersData, getCharacters, characterData, setCharacterData } = useContext(UserContext);
+    const { getCharacters, characterData, planetData , getPlanets} = useContext(UserContext);
 
     useEffect(() => {
         getCharacters();
+        getPlanets();
     }, []);
     return (<>
 
@@ -22,7 +23,7 @@ const Home = () => {
         </header>
 
         <section className="py-5" >
-
+            <h1> Characters: </h1>
             <div className="container px-4 px-lg-5 mt-5" >
 
                 <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center" >
@@ -46,12 +47,42 @@ const Home = () => {
                                 </div>
                             </div>
                         </div>
-                    ))};
+                    ))}
+                </div>
+            </div>
+        </section>
+        <section className="mt-0" >
+            <h1> Planets: </h1>
+            <div className="container px-4 px-lg-5 mt-5" >
+
+                <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center" >
+                    {planetData.map((planet) => (
+                        <div className="col mb-5" key={planet.result.uid}>
+                            <div className="card h-100">
+
+                                <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+
+                                <div className="card-body p-4">
+                                    <div className="text-center">
+
+                                        <h5 className="fw-bolder">{planet.result.properties.name}</h5>
+
+                                        Population: {planet.result.properties.population}
+                                    </div>
+                                </div>
+
+                                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                    <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">View details</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
 
             </div>
-
         </section>
+
+
     </>
     )
 };
--- src/js/store/UserContext.js
+++ src/js/store/UserContext.js
@@ -6,6 +6,8 @@ export const UserProvider = ({ children }) => {
   const [userData, setUserData] = useState([]);
   const [charactersData, setCharactersData] = useState([]);
 const [characterData, setCharacterData] = useState([]);
+const [planetsData, setPlanetsData] = useState([]);
+const [planetData, setPlanetData] = useState([]);
 
   const getCharacters = () => {
     const url = "https://www.swapi.tech/api/people/";
@@ -27,7 +29,7 @@ const [characterData, setCharacterData] = useState([]);
       })
       .then(data => {
         if (Array.isArray(data.results)) {
-          console.log("Data obtenida de la primera llamada GET:", data);
+          console.log("Characters obtenidos de la primera llamada GET:", data);
           setCharactersData(data.results);
 
           // Realizar llamadas GET para cada URL de personaje
@@ -62,6 +64,83 @@ const [characterData, setCharacterData] = useState([]);
         console.error("Error en la solicitud GET:", error);
       });
   };
+
+  const getPlanets = () => {
+    const url = "https://www.swapi.tech/api/planets";
+
+    const getOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+    fetch(url, getOptions)
+      .then(getResponse => {
+        if (getResponse.status >= 200 && getResponse.status < 300) {
+          console.log("GET: Planets cargados exitosamente");
+          return getResponse.json();
+        } else {
+          console.log(`Error en la solicitud GET ${getResponse.status}`);
+        }
+      })
+      .then(data => {
+        if (Array.isArray(data.results)) {
+          console.log("Planets obtenida de la primera llamada GET:", data);
+          setPlanetsData(data.results);
+
+          // Realizar llamadas GET para cada URL de planetas
+          const planetUrls = data.results.map(planet => planet.url);
+          const planetPromises = planetUrls.map(planetUrl =>
+            fetch(planetUrl, getOptions)
+              .then(planetResponse => {
+                if (planetResponse.status >= 200 && planetResponse.status < 300) {
+                  return planetResponse.json();
+                } else {
+                  console.log(`Error en la solicitud GET ${planetResponse.status}`);
+                  throw new Error("Error en la solicitud GET");
+                }
+              })
+          );
+
+          // Procesar los resultados de las llamadas GET individuales
+          Promise.all(planetPromises)
+            .then(planetDataArray => {
+              console.log("Detalles de los planetas:", planetDataArray);
+              // Almacenar la información de los personajes en el estado characterData
+              setPlanetData(planetDataArray);
+            })
+            .catch(error => {
+              console.error("Error en la solicitud GET:", error);
+            });
+        } else {
+          console.log("Error: Data.results no es un array.");
+        }
+      })
+      .catch(error => {
+        console.error("Error en la solicitud GET:", error);
+      });
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -108,7 +187,7 @@ const [characterData, setCharacterData] = useState([]);
   };
 
   return (
-    <UserContext.Provider value={{ charactersData, setCharactersData, getCharacters, characterData, setCharacterData, userData, setUserData, updateUserContact, addNewUserContact, getagenda }}>
+    <UserContext.Provider value={{ charactersData, setCharactersData, getCharacters, characterData, setCharacterData, planetsData, setPlanetsData, getPlanets, planetData, setPlanetData,userData, setUserData, updateUserContact, addNewUserContact, getagenda }}>
       {children}
     </UserContext.Provider>
   );
