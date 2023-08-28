f27081e3aa6f65f53cd3febebe11339477cde9fe
JuancarlosMuller <juancarlos.muller.b@gmail.com>
2023-08-28 18:14:50 -0400

    quinto

--- app.py
+++ app.py
@@ -34,9 +34,9 @@ class Planet(db.Model):
     __tablename__ = "planet"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250))
-    weather = db.Column(db.String(250))
-    land = db.Column(db.String(250), nullable=False)
-    population = db.Column(db.Integer)
+    climate = db.Column(db.String(250), nullable=True, default="Unknown")
+    terrain = db.Column(db.String(250), nullable=True, default="Unknown")
+    population = db.Column(db.Integer, nullable=True, default="Unknown")
 
 
 class Favorite(db.Model):
@@ -81,11 +81,48 @@ def get_external_character():
         db.session.commit()
         num_characters_added += 1
 
-    message = f"{num_characters_added} characters created from external source"
+    message = f"{num_characters_added} characters created from external API source"
     return jsonify({"message": message}), 201
 
 
-# Método GET para listar todos los personajes y id.
+# GET para obtener datos planetas desde Api Externa y alimentar mi Base de datos.
+@app.route("/external-planet", methods=["GET"])
+def get_external_planet():
+    response = requests.get("https://www.swapi.tech/api/planets/")
+    external_planet_data = response.json()
+
+    planet_list = external_planet_data["results"]
+    num_planets_added = 0  # Contador para el número de planetas agregados
+
+    for planet_data in planet_list:
+        planet_url = planet_data["url"]
+        planet_uid = planet_data["uid"]
+        planet_response = requests.get(planet_url)
+        planet_details = planet_response.json()["result"]["properties"]
+
+        population = planet_details["population"]
+        if population.lower() == "unknown":
+            population_int = None  # Asigno None para indicar un valor desconocido
+        else:
+            population_int = int(population)
+
+        new_planet = Planet(
+            name=planet_details["name"],
+            id=int(planet_uid),
+            climate=planet_details["climate"],
+            terrain=planet_details["terrain"],
+            population=population_int,
+        )
+
+        db.session.add(new_planet)
+        db.session.commit()
+        num_planets_added += 1
+
+    message = f"{num_planets_added} planets created from external API source"
+    return jsonify({"message": message}), 201
+
+
+# Método GET para listar todos los personajes, solo Nombre e ID.
 @app.route("/characters", methods=["GET"])
 def get_all_characters():
     characters = Character.query.all()
@@ -99,12 +136,12 @@ def get_all_characters():
     return jsonify(character_list)
 
 
-# Método GET para obtener el detalle de un personaje en particular.
+# Método GET para obtener el detalle de un personaje en particular segun id.
 @app.route("/character/<int:character_id>", methods=["GET"])
 def get_character_details(character_id):
     character = Character.query.get(character_id)
     if character is None:
-        return jsonify({"message": "Character not found"}), 404
+        return jsonify({"message": "Character id not found"}), 404
 
     character_details = {
         "id": character.id,
@@ -116,6 +153,38 @@ def get_character_details(character_id):
     }
 
     return jsonify(character_details)
+
+
+# Método GET para listar todos los planetas, solo Nombre e ID.
+@app.route("/planets", methods=["GET"])
+def get_all_planets():
+    planets = Planet.query.all()
+    planet_list = []
+    for planet in planets:
+        planet_data = {
+            "id": planet.id,
+            "name": planet.name,
+        }
+        planet_list.append(planet_data)
+    return jsonify(planet_list)
+
+
+# Método GET para obtener el detalle de un planeta en particular segun id.
+@app.route("/planet/<int:planet_id>", methods=["GET"])
+def get_planet_details(planet_id):
+    planet = Planet.query.get(planet_id)
+    if planet is None:
+        return jsonify({"message": "Planet id not found"}), 404
+
+    planet_details = {
+        "id": planet.id,
+        "name": planet.name,
+        "climate": planet.climate,
+        "terrain": planet.terrain,
+        "population": planet.population,
+    }
+
+    return jsonify(planet_details)
 
 
 if __name__ == "__main__":
