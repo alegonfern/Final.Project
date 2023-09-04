daf633d4afbc6ef898eb8958abce8665f76bdeea
JuancarlosMuller <juancarlos.muller.b@gmail.com>
2023-09-04 16:08:11 -0300

    actualizacion

--- models.py
+++ models.py
@@ -2,7 +2,9 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from datetime import datetime
+from flask_bcrypt import Bcrypt
 
+bcrypt = Bcrypt()
 db = SQLAlchemy()
 
 # Definicion del modelo de datos:
@@ -13,8 +15,11 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(250), nullable=False)
     mail = db.Column(db.String(250), nullable=False, unique=True)
-    password = db.Column(db.String(250), nullable=False)
+    password_hash = db.Column(db.String(250), nullable=False)
     suscription_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
 
 
 class Character(db.Model):
