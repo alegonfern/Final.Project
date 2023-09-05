188dded9b1f7e410bf50ddc939e70110e8011a8e
Eduardo Orellana <123591780+EOrellanaVasquez@users.noreply.github.com>
2023-09-04 23:05:52 -0300

    Actualización Home
    Creación componente Carousel donde se renderizarán los grupos

---
+++ src/js/component/Carousel.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Carousel = () => {
+  return (
+    <div>
+      Componente para renderizar grupos
+    </div>
+  )
+}
+
+export default Carousel
--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useState } from 'react';
 import Post from './Post';
 
 const Home = () => {
