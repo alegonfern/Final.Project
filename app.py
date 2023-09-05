4c58f896fe43850ee22ffd8126e8db78d584174d
JuancarlosMuller <juancarlos.muller.b@gmail.com>
2023-09-05 01:45:12 -0300

    User Virtual

--- app.py
+++ app.py
@@ -2,21 +2,15 @@ from flask import Flask, jsonify, request
 from models import User
 import requests
 from models import db
-from werkzeug.security import check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_admin import Admin
-from flask_admin.contrib.sqla import ModelView
-from flask_cors import CORS
-from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
-
+from flask_admin.contrib.sqla import ModelView
 app = Flask(__name__)
-bcrypt = Bcrypt(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
 app.config["SECRET_KEY"] = "123456"  # Mi propia clave secreta
 db.init_app(app)
 
-# Configura CORS para permitir solicitudes desde cualquier origen "*"
-CORS(app, resources={r"/login": {"origins": "*"}})
 
 # Configura migraciones
 migrate = Migrate(app, db)  # Configura las migraciones
@@ -24,6 +18,21 @@ migrate = Migrate(app, db)  # Configura las migraciones
 # Configuracion Flask-Admin
 admin = Admin(app, name="Admin", template_mode="bootstrap3")
 admin.add_view(ModelView(User, db.session))
+
+
+# Middleware para manejar CORS manualmente
+@app.after_request
+def add_cors_headers(response):
+    response.headers[
+        "Access-Control-Allow-Origin"
+    ] = "*"  # Permite solicitudes desde cualquier origen
+    response.headers[
+        "Access-Control-Allow-Methods"
+    ] = "GET, POST, PUT, DELETE, OPTIONS"  # Métodos permitidos
+    response.headers[
+        "Access-Control-Allow-Headers"
+    ] = "Content-Type, Authorization"  # Encabezados permitidos
+    return response
 
 
 # validacion de Inicio de Sesion con metodo POST.
@@ -35,7 +44,7 @@ def login():
 
     user = User.query.filter_by(username=username).first()
 
-    if user and bcrypt.check_password_hash(user.password_hash, password):
+    if user and check_password_hash(user.password_hash, password):
         # Autenticación exitosa
         return jsonify({"message": "Autenticación exitosa"})
     else:
--- models.py
+++ models.py
@@ -1,9 +1,8 @@
-from app import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from datetime import datetime
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
@@ -19,5 +18,7 @@ class User(db.Model):
     suscription_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def set_password(self, password):
-        bcrypt = Flask().bcrypt
-        self.password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
