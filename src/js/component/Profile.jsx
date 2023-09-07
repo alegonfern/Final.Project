0c90a863069ea680b66b854cbd7d21eb0bf175bd
alegonfern <alegonfern@gmail.com>
2023-09-07 09:59:59 -0300

    7-9

--- src/js/component/Profile.jsx
+++ src/js/component/Profile.jsx
@@ -7,14 +7,14 @@ const Profile = () => {
 
             <div className="container">
 
-                <div class="card mb-3 border-0 pt-5" style={{ width: "420px" }}>
-                    <div class="row g-0">
-                        <div class="col-md-4">
+                <div className="card mb-3 border-0 pt-5" style={{ width: "420px" }}>
+                    <div className="row g-0">
+                        <div className="col-md-4">
                             <img src="https://img.freepik.com/iconos-gratis/usuario_318-875902.jpg" class="img-fluid rounded-circle" alt="Avatar del usuario" />
                         </div>
-                        <div class="col-md-8 d-flex align-items-end">
-                            <div class="card-body">
-                                <h5 class="card-title">Nombre usuario</h5>
+                        <div className="col-md-8 d-flex align-items-end">
+                            <div className="card-body">
+                                <h5 className="card-title">Nombre usuario</h5>
                             </div>
                         </div>
                     </div>
