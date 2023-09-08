dd5bed64a911213ddeb19be0d4ed2b8648de6b85
JuancarlosMuller <juancarlos.muller.b@gmail.com>
2023-09-08 06:03:57 -0300

    Macth Views

--- src/js/component/Match.jsx
+++ src/js/component/Match.jsx
@@ -1,24 +1,50 @@
 import React from 'react';
 import '../../styles/Match.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 
-
-
-function Match() {
-    const randomImage1 = `https://picsum.photos/seed/${Math.random()}/100`;
-    const randomImage2 = `https://picsum.photos/seed/${Math.random()}/100`;
+const Match = () => {
+    // Genera URLs aleatorias para las imágenes de avatar (simula imágenes aleatorias)
+    const getRandomAvatarUrl = () => {
+        const randomNum = Math.floor(Math.random() * 1000);
+        return `https://picsum.photos/200/300?random=${randomNum}`;
+    };
 
     return (
-        <div className="match-screen">
-            <h1>ADN MATCH!</h1>
-            <div className="profile-pictures">
-                <img src={randomImage1} alt="Gamer 1" />
-                <img src="joystick.png" alt="Joystick" />
-                <img src={randomImage2} alt="Gamer 2" />
+        <div className="match-view">
+            <div className="header">
+                <h1>¡Match!</h1>
+                <div className="avatars">
+                    <img src={getRandomAvatarUrl()} />
+                    <img src={getRandomAvatarUrl()} />
+                </div>
             </div>
-            <button>Send a Message</button>
-            <button>Keep Playing</button>
+            <div className="gamers">
+                <div className="gamer-card">
+                    <img src={getRandomAvatarUrl()} />
+                    <h2>Nombre 1</h2>
+                    <p>XXXXX</p>
+                    <button className="btn btn-primary">
+                        <FontAwesomeIcon icon={faEnvelope} /> Enviar mensaje
+                    </button>
+                    <button className="btn btn-success">
+                        <FontAwesomeIcon icon={faUserPlus} /> Agregar como amigo
+                    </button>
+                </div>
+                <div className="gamer-card">
+                    <img src={getRandomAvatarUrl()} />
+                    <h2>Nombre 2</h2>
+                    <p>xxxxxxxx</p>
+                    <button className="btn btn-primary">
+                        <FontAwesomeIcon icon={faEnvelope} /> Enviar mensaje
+                    </button>
+                    <button className="btn btn-success">
+                        <FontAwesomeIcon icon={faUserPlus} /> Agregar como amigo
+                    </button>
+                </div>
+            </div>
         </div>
     );
-}
+};
 
 export default Match;
--- src/js/component/MatchPreview.jsx
+++ src/js/component/MatchPreview.jsx
@@ -1,33 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../../styles/MatchPreview.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faTimes, faDna } from '@fortawesome/free-solid-svg-icons';
 
-const MatchPreview = ({ name, interests, profilePicture, backgroundPicture }) => {
+const MatchPreview = () => {
+  const [liked, setLiked] = useState(false);
+  const name = "Nombre del usuario";
+  const age = 25;
+  const location = "Santiago, Chile";
+  const gameStats = [
+    { genre: 'Acción', game: 'Juego 1' },
+    { genre: 'Estrategia', game: 'Juego 2' },
+  ];
+
+  const handleLike = () => {
+    setLiked(true);
+    setTimeout(() => setLiked(false), 1000);
+  };
+
   return (
-    <div className="card" style={{ width: '18rem' }}>
-      <img src={backgroundPicture} className="card-img-top" alt="Background" />
-      <div className="card-body">
-        <div className="d-flex justify-content-center">
-          <img
-            src={profilePicture}
-            alt="Profile"
-            style={{
-              width: '100px',
-              height: '100px',
-              borderRadius: '50%',
-              objectFit: 'cover',
-            }}
-          />
+    <div className="container">
+      <div className="card profile-card">
+        <img
+          src="https://picsum.photos/150"
+          className="card-img-top profile-image"
+          alt="Profile"
+        />
+        <div className="card-body">
+          <h5 className="card-title profile-name">{name}</h5>
+          <p className="card-text profile-info">
+            {age} años - {location}
+          </p>
+          <table className="table table-dark table-sm game-stats">
+            <thead>
+              <tr>
+                <th scope="col">Género</th>
+                <th scope="col">Juego</th>
+              </tr>
+            </thead>
+            <tbody>
+              {gameStats.map((stat) => (
+                <tr key={stat.game}>
+                  <td>{stat.genre}</td>
+                  <td>{stat.game}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-        <h5 className="card-title text-center mt-2">{name}</h5>
-      </div>
-      <ul className="list-group list-group-flush">
-        <li className="list-group-item">Interests: {interests}</li>
-      </ul>
-      <div className="card-body d-flex justify-content-between">
-        <button type="button" className="btn btn-danger">X</button>
-        <button type="button" className="btn btn-success">♥️</button>
+        <div className="card-footer bg-transparent d-flex justify-content-around">
+          <button className="btn btn-danger">
+            <FontAwesomeIcon icon={faTimes} />
+          </button>
+          <button className="btn btn-success" onClick={handleLike}>
+            <FontAwesomeIcon icon={faDna} />
+          </button>
+        </div>
+        {liked && (
+          <div className="dna-animation">
+            <FontAwesomeIcon icon={faDna} size="4x" />
+          </div>
+        )}
       </div>
     </div>
   );
 };
+
 
 export default MatchPreview;
