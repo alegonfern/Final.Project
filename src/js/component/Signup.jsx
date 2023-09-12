723963385420b2fc9fd1c0b672611fc9e0f9ae6a
Eduardo Orellana <123591780+EOrellanaVasquez@users.noreply.github.com>
2023-09-12 20:15:06 -0300

    commit
    Se centró formulario de registro

--- src/js/component/Signup.jsx
+++ src/js/component/Signup.jsx
@@ -1,14 +1,10 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react';
 
 const Signup = () => {
-
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [userName, setUsername] = useState('');
-
 
     const url = "http://127.0.0.1:5000/signup";
 
@@ -20,16 +16,14 @@ const Signup = () => {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ firstName, lastName, email, userName }),
+            body: JSON.stringify({ firstName, lastName, email }),
         };
 
         fetch(url, postOptions)
             .then(response => {
                 if (response.ok) {
-                    // Creación de usuario exitosa, redirige a creación de perfil
                     window.location.href = '/profile';
                 } else {
-                    // Creación fallida por algún motivo
                     console.error('Revisa los datos ingresados');
                 }
             })
@@ -38,143 +32,63 @@ const Signup = () => {
             });
     };
 
-
-
     return (
-        <div className="container-fluid">
-            <div className="col-md-6 col-lg-6 col-sm-6 offset-md-3 border rounded">
+        <div className="container">
+            <div className="form-container col-12 col-md-8 col-lg-6 border rounded">
                 <div className=' card singup_form p-3'><h3>¡Regístrate, ven a jugar con nosotros!</h3><p>No lo lamentarás.</p></div>
 
                 <form className="row g-3 m-2" onSubmit={handleSubmit}>
-                    <div className="col-md-6"col-lg-6 col-sm-6 >
-                        <label for="input_first_name" className="form-label">Nombre</label>
-                        <input type="text" id="first_name" value={firstName} onChange={(e) => setFirstName(e.target.value)} required />
+                    <div className="col-md-6">
+                        <label for="first_name" className="form-label">Nombre</label>
+                        <input type="text" className="form-control" id="first_name" value={firstName} onChange={(e) => setFirstName(e.target.value)} required />
                     </div>
-                    <div className="col-md-6"col-lg-6 col-sm-6 >
-                        <label for="input_last_name" className="form-label">Apellido</label>
-                        <input type="text" id="last_name" value={lastName} onChange={(e) => setLastName(e.target.value)} required />
+                    <div className="col-md-6">
+                        <label for="last_name" className="form-label">Apellido</label>
+                        <input type="text" className="form-control" id="last_name" value={lastName} onChange={(e) => setLastName(e.target.value)} required />
                     </div>
-                    <div className="col-md-6"col-lg-6 col-sm-6 >
-                        <label for="input_email" className="form-label">Email</label>
-                        <br />
-                        <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                    <div className="col-md-6">
+                        <label for="email" className="form-label">Email</label>
+                        <input type="email" className="form-control" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
                     </div>
-                    <div className="col-md-6"col-lg-6 col-sm-6 >
-                        <label for="input_confirm_email" className="form-label">Confirmar email</label>
-                        <br />
-                        <input type="email" id="confirm_email" value={email} />
+                    <div className="col-md-6">
+                        <label for="confirm_email" className="form-label">Confirmar email</label>
+                        <input type="email" className="form-control" id="confirm_email" value={email} />
                     </div>
-                    <div className="col-md-12 col-lg-12 col-sm-12">
-                        <label for="input_password" className="form-label">Nombre de usuario</label>
-                        <br />
-                        <input type="text" id="user_name" value={userName} onChange={(e) => setUsername(e.target.value)} required />
+                    <div className="col-md-6">
+                        <label for="password" className="form-label">Contraseña</label>
+                        <input type="password" className="form-control" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
                     </div>
-                    <div className="col-md-6"col-lg-6 col-sm-6 >
-                        <label for="input_password" className="form-label">Contraseña</label>
-                        <br />
-                        <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-                    </div>
-                    <div className="col-md-6"col-lg-6 col-sm-6 >
-                        <label for="input_confirm_password" className="form-label">Confirmar contraseña</label>
-                        <br />
+                    <div className="col-md-6">
+                        <label for="confirm_password" className="form-label">Confirmar contraseña</label>
                         <input type="password" className="form-control" id="confirm_password" value={password} />
                     </div>
-
-
-                    <label for="select_birth_day" className="form-label">Fecha de nacimiento</label>
-                    <div className="col-2">
-                        <select className="form-select" aria-label="Select day">
-                            <option selected>1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
-                            <option value="7">7</option>
-                            <option value="8">8</option>
-                            <option value="9">9</option>
-                            <option value="10">10</option>
-                            <option value="11">11</option>
-                            <option value="12">12</option>
-                            <option value="13">13</option>
-                            <option value="14">14</option>
-                            <option value="15">15</option>
-                            <option value="16">16</option>
-                            <option value="17">17</option>
-                            <option value="18">18</option>
-                            <option value="19">19</option>
-                            <option value="20">20</option>
-                            <option value="21">21</option>
-                            <option value="22">22</option>
-                            <option value="23">23</option>
-                            <option value="24">24</option>
-                            <option value="25">25</option>
-                            <option value="26">26</option>
-                            <option value="27">27</option>
-                            <option value="28">28</option>
-                            <option value="29">29</option>
-                            <option value="30">30</option>
-                            <option value="31">31</option>
-                        </select>
-                    </div>
-                    <div className="col-2">
-                        <select className="form-select" aria-label="Select month">
-                            <option selected>Enero</option>
-                            <option value="2">Febrero</option>
-                            <option value="3">Marzo</option>
-                            <option value="4">Abril</option>
-                            <option value="5">Mayo</option>
-                            <option value="6">Junio</option>
-                            <option value="7">Julio</option>
-                            <option value="8">Agosto</option>
-                            <option value="9">Septiembre</option>
-                            <option value="10">Octubre</option>
-                            <option value="11">Noviembre</option>
-                            <option value="12">Diciembre</option>
-                        </select>
-                    </div>
-                    <div className="col-2">
-                        <select className="form-select" aria-label="Select year">
-                            <option selected>2023</option>
-                            <option value="2">2022</option>
-                            <option value="3">2021</option>
-                            <option value="4">2020</option>
-                            <option value="5">2019</option>
-                            <option value="6">2018</option>
-                            <option value="7">2017</option>
-                            <option value="8">2016</option>
-                            <option value="9">2015</option>
-                            <option value="10">2014</option>
-                            <option value="11">2013</option>
-                            <option value="12">2012</option>
-                        </select>
+                    <div className='col-md-6 offset-md-3'>
+                      <label for='birth_date' class='form-label'>Fecha de nacimiento</label>
+                      <input type='date' class='form-control' id='birth_date' required />
                     </div>
 
-                    <div >
-                        <label for="select_birth_day" className="form-label">Sexo</label>
-                        <div className="col-12">
-                            <div className="form-check form-check-inline">
-                                <input className="select_gender_radio" type="radio" name="gender_radio" id="male_radio" value="Male" />
-                                <label className="form-check-label" for="male_radio">Hombre</label>
-                            </div>
-                            <div className="form-check form-check-inline">
-                                <input className="select_gender_radio" type="radio" name="gender_radio" id="female_radio" value="Female" />
-                                <label className="form-check-label" for="female_radio">Mujer</label>
-                            </div>
-                            <div className="form-check form-check-inline">
-                                <input className="select_gender_radio" type="radio" name="gender_radio" id="non_binary" value="Non binary" checked />
-                                <label className="form-check-label" for="non_binary">No binario</label>
-                            </div>
-                        </div>
+                    <label for="" class='form-label'>Sexo</label>
+                    <div class='col-12'>
+                      <div class='form-check form-check-inline'>
+                          <input class='select_gender_radio' type='radio' name='gender_radio' id='male_radio' value='Male' />
+                          <label class='form-check-label' for='male_radio'>Hombre</label>
+                      </div>
+                      <div class='form-check form-check-inline'>
+                          <input class='select_gender_radio' type='radio' name='gender_radio' id='female_radio' value='Female' />
+                          <label class='form-check-label' for='female_radio'>Mujer</label>
+                      </div>
+                      <div class='form-check form-check-inline'>
+                          <input class='select_gender_radio' type='radio' name='gender_radio' id='non_binary' value='Non binary' checked />
+                          <label class='form-check-label' for='non_binary'>No binario</label>
+                      </div>
                     </div>
-                    <div className="col-12">
-                        <button type="submit" className="btn btn-dark d-grid gap-2 col-2 mx-auto">¡M4tchiemos!</button>
+                    <div class='col-md-6 offset-md-3'>
+                      <button type='submit' class='btn btn-dark d-grid gap-2 mx-auto'>¡M4tchiemos!</button>
                     </div>
                 </form>
             </div>
         </div>
+    );
+};
 
-    )
-}
-
-export default Signup
+export default Signup;
