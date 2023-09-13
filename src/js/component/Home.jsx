84399c35c224da1cbc0d4d33899e7b50fe826dce
Eduardo Orellana <123591780+EOrellanaVasquez@users.noreply.github.com>
2023-09-12 23:09:30 -0300

    commit
    ajustando vista home

--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -1,30 +1,28 @@
 import React from 'react'
-import Carousel from './Carousel';
+import HomeGroupCarousel from "./home component/HomeGroupCarousel"
 import PostList from './post components/PostList';
 import ProfileCard from './ProfileCard';
 
 const Home = () => {
-
-
     return (
-
-        <div className='row'>
-            <div className="container-fluid">
-                <div className="container border-top border-muted my-5">
-
-                    <ProfileCard />
-
-                    <Carousel />
-
-                    <div className='post-container'>
-                        <h3>Bitácora</h3>
-                        <PostList />
+        <div className="container-fluid mt-4 pt-5"> {/* Agregué un padding-top aquí */}
+            <div className="container border-top border-muted my-5">
+                <div className="row">
+                    <div className="col-12 order-1 order-md-1">
+                        <ProfileCard />
+                    </div>
+                    <div className="col-12 order-2 order-md-2 d-flex flex-column">
+                        <HomeGroupCarousel className="order-1 flex-grow-1 w-100 mt-3" />
+                    </div>
+                    <div className="col-12 order-3 order-md-3">
+                        <div className='post-container'>
+                            <h3>Bitácora</h3>
+                            <PostList />
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-
-
     )
 }
 
