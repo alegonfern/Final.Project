2c17731568063b6801c794f7741bb3f39c9379a7
alegonfern <alegonfern@gmail.com>
2023-09-14 01:59:32 -0300

    sidebar funcional faltan otros items

--- src/js/component/Sidebar.jsx
+++ src/js/component/Sidebar.jsx
@@ -6,6 +6,10 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faHeart, faComments, faList, faUserFriends, faBell, faStar } from '@fortawesome/free-solid-svg-icons';
 
+
+
+
+
 function Sidebar() {
     const { theme, toggleTheme } = useContext(UserContext);
     const [checked, setChecked] = useState(theme === 'dark');
@@ -16,50 +20,55 @@ function Sidebar() {
 
     return (
         <div className={`bg-dark text-white p-3 sidebar-container ${theme}`}>
-        <ul className="nav flex-column">
-          <li className="nav-item">
-            <a className="nav-link active" href="#">
-              <FontAwesomeIcon icon={faUser} /> Mi Perfil
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              <FontAwesomeIcon icon={faHeart} /> Mis Intereses
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              <FontAwesomeIcon icon={faList} /> Salas de Interés
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              <FontAwesomeIcon icon={faUserFriends} /> Mis M4tch's
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              <FontAwesomeIcon icon={faComments} /> Chats
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              <FontAwesomeIcon icon={faBell} /> Eventos
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              <FontAwesomeIcon icon={faStar} /> Valoración
-            </a>
-          </li>
-        </ul>
-        <div>
-          <label className="switch">
-            <input type="checkbox" onClick={toggleTheme} checked={checked} />
-            <span className={`slider round ${theme === 'dark' ? 'dark' : ''}`}></span>
-          </label>
+            <ul className="nav flex-column">
+                <li className="nav-item">
+                    <Link to="/Home" className="nav-link">
+                        <FontAwesomeIcon icon={faUser} /> Mi Perfil
+                    </Link>
+                </li>
+
+                <li className="nav-item">
+                    <Link to="/Intereses" className="nav-link">
+                        <FontAwesomeIcon icon={faHeart} /> Mis Intereses
+                    </Link>
+
+                </li>
+
+
+
+                <li className="nav-item">
+                    <a className="nav-link" href="#">
+                        <FontAwesomeIcon icon={faList} /> Salas de Interés
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">
+                        <FontAwesomeIcon icon={faUserFriends} /> Mis M4tch's
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">
+                        <FontAwesomeIcon icon={faComments} /> Chats
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">
+                        <FontAwesomeIcon icon={faBell} /> Eventos
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">
+                        <FontAwesomeIcon icon={faStar} /> Valoración
+                    </a>
+                </li>
+            </ul>
+            <div>
+                <label className="switch">
+                    <input type="checkbox" onClick={toggleTheme} checked={checked} />
+                    <span className={`slider round ${theme === 'dark' ? 'dark' : ''}`}></span>
+                </label>
+            </div>
         </div>
-      </div>
     );
 }
 
