e93e6795192de4d1a9ff3f3b6e685c9cbd5a8d59
Eduardo Orellana <123591780+EOrellanaVasquez@users.noreply.github.com>
2023-09-24 01:41:01 -0300

    commit
    se agregó en home un espacio para traer datos en el footer

---
+++ src/js/component/home/HomeFooter.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const HomeFooter = () => {
+    return (
+        <>
+            <header className="bg-dark my-5 p-5">
+
+                <div className="card-group">
+
+                    <div className="card px-4 py-4">
+                        <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <p className="card-text">
+                                <small className="text-body-secondary">Descripción gráfico </small>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="card px-4 py-4">
+                        <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <p className="card-text">
+                                <small className="text-body-secondary">Descripción gráfico </small>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="card px-4 py-4">
+                        <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <p className="card-text">
+                                <small className="text-body-secondary">Descripción gráfico </small>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+            </header>
+        </>
+    )
+}
+
+export default HomeFooter
