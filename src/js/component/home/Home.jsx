baa9464e7aaa7fbdfc503b037a6ccaf5b317d4b4
Eduardo Orellana <123591780+EOrellanaVasquez@users.noreply.github.com>
2023-09-24 15:18:42 -0300

    Commit
    modificación a componente Home para renderizar vistas

--- src/js/component/home/Home.jsx
+++ src/js/component/home/Home.jsx
@@ -2,71 +2,110 @@ import React from 'react'
 import HomeCard from './HomeCard'
 import HomeFooter from './HomeFooter'
 
+import universoImg from '../../../img/generos de juegos/Universo.jpg';
+import accionImg from '../../../img/generos de juegos/Acción.jpg';
+import aventuraImg from '../../../img/generos de juegos/Aventura.jpg';
+import estrategiaImg from '../../../img/generos de juegos/Estrategia.jpg';
+import deportesImg from '../../../img/generos de juegos/Deportes.jpg';
+import carrerasImg from '../../../img/generos de juegos/Carreras.jpg';
+import simulacionImg from '../../../img/generos de juegos/Simulación.jpg';
+import rpgImg from '../../../img/generos de juegos/RPG.jpg';
+import plataformasImg from '../../../img/generos de juegos/Plataformas.jpg';
+import luchaImg from '../../../img/generos de juegos/Lucha.jpg';
+import shooterImg from '../../../img/generos de juegos/Shooter.jpg';
+import sandboxImg from '../../../img/generos de juegos/Sandbox.jpg';
+import estrategiaEnTiempoRealImg from '../../../img/generos de juegos/Estrategia en tiempo real.jpg';
+import mobaImg from '../../../img/generos de juegos/Multijugador en línea Battle Arena (MOBA).jpg';
+import juegosDeRolImg from '../../../img/generos de juegos/Juegos de rol (RPG, ARPG y más).jpg';
+import simulacionYDeportesImg from '../../../img/generos de juegos/Simulación y deportes.jpg';
+import rompecabezasYJuegosDeFiestaImg from '../../../img/generos de juegos/Rompecabezas y juegos de fiesta.jpg';
+import accionAventuraImg from '../../../img/generos de juegos/Acción-aventura.jpg';
+import survivalImg from '../../../img/generos de juegos/Survival.jpg';
+import horrorImg from '../../../img/generos de juegos/Horror.jpg';
+import cienciaFiccionImg from '../../../img/generos de juegos/Ciencia ficción.jpg';
+import fantasiaImg from '../../../img/generos de juegos/Fantasía.jpg';
+import misterioImg from '../../../img/generos de juegos/Misterio.jpg';
+import historiaVisualImg from '../../../img/generos de juegos/Historia visual.jpg';
+import musicalImg from '../../../img/generos de juegos/Musical.jpg';
+import educativoImg from '../../../img/generos de juegos/Educativo.jpg';
+import casualImg from '../../../img/generos de juegos/Casual.jpg';
 
 const Home = () => {
+
+    const groupImages = {
+        Universo: universoImg,
+        Acción: accionImg,
+        Aventura: aventuraImg,
+        Estrategia: estrategiaImg,
+        Deportes: deportesImg,
+        Carreras: carrerasImg,
+        Simulación: simulacionImg,
+        RPG: rpgImg,
+        Plataformas: plataformasImg,
+        Lucha: luchaImg,
+        Shooter: shooterImg,
+        Sandbox: sandboxImg,
+        'Estrategia en tiempo real': estrategiaEnTiempoRealImg,
+        'Multijugador en línea Battle Arena (MOBA)': mobaImg,
+        'Juegos de rol (RPG, ARPG y más)': juegosDeRolImg,
+        'Simulación y deportes': simulacionYDeportesImg,
+        'Rompecabezas y juegos de fiesta': rompecabezasYJuegosDeFiestaImg,
+        'Acción-aventura': accionAventuraImg,
+        Survival: survivalImg,
+        Horror: horrorImg,
+        'Ciencia ficción': cienciaFiccionImg,
+        Fantasía: fantasiaImg,
+        Misterio: misterioImg,
+        'Historia visual': historiaVisualImg,
+        Musical: musicalImg,
+        Educativo: educativoImg,
+        Casual: casualImg,
+    };
+
+    const groupsData = [
+        { groupName: 'Universo', groupImagen: groupImages.Universo },
+        { groupName: 'Acción', groupImagen: groupImages.Acción },
+        { groupName: 'Aventura', groupImagen: groupImages.Aventura },
+        { groupName: 'Estrategia', groupImagen: groupImages.Estrategia },
+        { groupName: 'Deportes', groupImagen: groupImages.Deportes },
+        { groupName: 'Carreras', groupImagen: groupImages.Carreras },
+        { groupName: 'Simulación', groupImagen: groupImages.Simulación },
+        { groupName: 'RPG', groupImagen: groupImages.RPG },
+        { groupName: 'Plataformas', groupImagen: groupImages.Plataformas },
+        { groupName: 'Lucha', groupImagen: groupImages.Lucha },
+        { groupName: 'Shooter', groupImagen: groupImages.Shooter },
+        { groupName: 'Sandbox', groupImagen: groupImages.Sandbox },
+        { groupName: 'Estrategia en tiempo real', groupImagen: groupImages['Estrategia en tiempo real'] },
+        { groupName: 'Multijugador en línea Battle Arena (MOBA)', groupImagen: groupImages['Multijugador en línea Battle Arena (MOBA)'] },
+        { groupName: 'Juegos de rol (RPG, ARPG y más)', groupImagen: groupImages['Juegos de rol (RPG, ARPG y más)'] },
+        { groupName: 'Simulación y deportes', groupImagen: groupImages['Simulación y deportes'] },
+        { groupName: 'Rompecabezas y juegos de fiesta', groupImagen: groupImages['Rompecabezas y juegos de fiesta'] },
+        { groupName: 'Acción-aventura', groupImagen: groupImages['Acción-aventura'] },
+        { groupName: 'Survival', groupImagen: groupImages.Survival },
+        { groupName: 'Horror', groupImagen: groupImages.Horror },
+        { groupName: 'Ciencia ficción', groupImagen: groupImages['Ciencia ficción'] },
+        { groupName: 'Fantasía', groupImagen: groupImages.Fantasía },
+        { groupName: 'Misterio', groupImagen: groupImages.Misterio },
+        { groupName: 'Historia visual', groupImagen: groupImages['Historia visual'] },
+        { groupName: 'Musical', groupImagen: groupImages.Musical },
+        { groupName: 'Educativo', groupImagen: groupImages.Educativo },
+        { groupName: 'Casual', groupImagen: groupImages.Casual },
+    ];
 
     return (
         <>
             <section className="py-5">
                 <div className="container px-4 px-lg-5 mt-5">
                     <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-
-                        <HomeCard groupImagen={"https://services.meteored.com/img/article/para-qual-lugar-o-universo-esta-se-expandindo-1685829316554_1280.png"}
-                            groupName={"Universo"} />
-                        <HomeCard groupImagen={"https://sm.ign.com/t/ign_es/screenshot/default/wallpapersden_rasy.1280.jpg"}
-                            groupName={"Acción"} />
-                        <HomeCard groupImagen={"https://www.hd-tecnologia.com/imagenes/articulos/2022/04/Top-10-de-los-mejores-juegos-de-aventura.jpg"}
-                            groupName={"Aventura"} />
-                        <HomeCard groupImagen={"https://ipadizate.com/hero/2022/01/Los-8-mejores-juegos-de-estrategia-que-puedes-descargar-en-el-iPad.jpg?width=1200"}
-                            groupName={"Estrategia"} />
-                        <HomeCard groupImagen={"https://sm.ign.com/t/ign_es/screenshot/default/wp4519264_56pw.1280.jpg"}
-                            groupName={"Deportes"} />
-                        <HomeCard groupImagen={"https://frontalgamer.com/wp-content/uploads/2022/01/Mejores-juegos-de-carreras-para-movil-1024x576.jpg"}
-                            groupName={"Carreras"} />
-                        <HomeCard groupImagen={"https://tus-videojuegos.com/wp-content/uploads/2020/07/simulacion.jpg"}
-                            groupName={"Simulación"} />
-                        <HomeCard groupImagen={"https://sm.ign.com/t/ign_es/screenshot/default/977818_8mjp.1280.jpg"}
-                            groupName={"RPG"} />
-                        <HomeCard groupImagen={"https://www.nintenderos.com/wp-content/uploads/2022/11/plataformas.jpg"}
-                            groupName={"Plataformas"} />
-                        <HomeCard groupImagen={"https://areajugones.sport.es/wp-content/uploads/2020/04/mejorsjuegoslucha.jpg"}
-                            groupName={"Lucha"} />
-                        <HomeCard groupImagen={"https://bitwares.net/wp-content/uploads/2021/08/Shooters-Gratuitos-Competitivos-Bitwares.jpg"}
-                            groupName={"Shooter"} />
-                        <HomeCard groupImagen={"https://planetanft.com/wp-content/uploads/2022/01/que-es-the-sandbox.jpg"}
-                            groupName={"Snadbox"} />
-                        <HomeCard groupImagen={"https://imgmedia.larepublica.pe/640x371/larepublica/original/2022/06/20/62b0e2759a835259d0292525.webp"}
-                            groupName={"Estrategia en tiempo real"} />
-                        <HomeCard groupImagen={"https://i.ytimg.com/vi/MxR5eKYM1Bc/maxresdefault.jpg"}
-                            groupName={"Multijugador en línea Battle Arena (MOBA)"} />
-                        <HomeCard groupImagen={"https://sm.ign.com/ign_es/screenshot/default/witcher-3_pwuc.jpg"}
-                            groupName={"Juegos de rol (RPG, ARPG y más)"} />
-                        <HomeCard groupImagen={"https://www.somosxbox.com/wp-content/uploads/2023/02/Football-manager-2023-scaled.jpg"}
-                            groupName={"Simulación y deportes"} />
-                        <HomeCard groupImagen={"https://gobookmart.com/wp-content/uploads/2023/03/9-65.jpg"}
-                            groupName={"Rompecabezas y juegos de fiesta"} />
-                        <HomeCard groupImagen={"https://www.mundodeportivo.com/alfabeta/hero/2021/03/immortal-fenyx-rising.jpg?width=1200&aspect_ratio=16:9"}
-                            groupName={"Acción-aventura"} />
-                        <HomeCard groupImagen={"https://cdn.ligadegamers.com/imagenes/mejores-juegos-supervivencia-pc-og.jpg"}
-                            groupName={"Survival"} />
-                        <HomeCard groupImagen={"https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2022/11/mejores-juegos-terror-2022-2872705.jpg?tf=3840x"}
-                            groupName={"Horror"} />
-                        <HomeCard groupImagen={"https://lasoga.org/wp-content/uploads/2014/08/ME-Slide.jpeg"}
-                            groupName={"Ciencia ficción"} />
-                        <HomeCard groupImagen={"https://s1.1zoom.me/big0/830/Fantastic_world_Battleborn_531776_1280x720.jpg"}
-                            groupName={"Fantasía"} />
-                        <HomeCard groupImagen={"https://articles-img.sftcdn.net/f_auto,t_article_cover_xl/auto-mapping-folder/sites/2/2019/02/juegos-misterio-header-2.jpg"}
-                            groupName={"Misterio"} />
-                        <HomeCard groupImagen={"https://s3.abcstatics.com/media/tecnologia/2018/12/11/mejores-videojuegos-2018-montaje-kHzF--620x349@abc.jpg"}
-                            groupName={"Historia visual"} />
-                        <HomeCard groupImagen={"https://www.mundodeportivo.com/alfabeta/hero/2021/05/guitar-hero-1.jpg?width=768&aspect_ratio=16:9&format=nowebp"}
-                            groupName={"Musical"} />
-                        <HomeCard groupImagen={"https://amexvid.com/wp-content/uploads/2019/04/juegos_educativos.png"}
-                            groupName={"Educativo"} />
-                        <HomeCard groupImagen={"https://nucleovisual.com/wp-content/uploads/2023/03/los-8-mejores-juegos-casuales-para-android.jpg"}
-                            groupName={"Casual"} />
+                        {groupsData.map((group, index) => (
+                            <HomeCard
+                                key={index}
+                                groupImagen={group.groupImagen}
+                                groupName={group.groupName}
+                            />
+                        ))}
                     </div>
-                    
+
                     <div className="row justify-content-center">
                         <HomeFooter />
                     </div>
