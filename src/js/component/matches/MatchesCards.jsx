e4306a44e5e6511538bf1e686b3481af859721ac
JuancarlosMuller <juancarlos.muller.b@gmail.com>
2023-09-25 02:06:53 -0300

    Macthes con ADN

--- src/js/component/matches/MatchesCards.jsx
+++ src/js/component/matches/MatchesCards.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState, useContext } from 'react';
-import { UserContext } from '../../store/UserContext'; // Importa tu contexto aquí
+import { UserContext } from '../../store/UserContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBirthdayCake } from '@fortawesome/free-solid-svg-icons';
+import { FaDna } from 'react-icons/fa';
 
 const GroupCards = () => {
     const [matches, setMatches] = useState([]);
+    const [compatibilities, setCompatibilities] = useState([]); // Estado para almacenar las compatibilidades
     const { userId } = useContext(UserContext); // Obtén el userId del contexto
 
     useEffect(() => {
@@ -17,7 +19,27 @@ const GroupCards = () => {
             .catch((error) => {
                 console.error("Error al obtener los matches:", error);
             });
+
+        // Realiza una solicitud GET para obtener las compatibilidades
+        fetch(`http://127.0.0.1:5000/calcular_compatibilidad_entre_usuarios`)
+            .then((response) => response.json())
+            .then((data) => {
+                setCompatibilities(data.compatibilidades);
+            })
+            .catch((error) => {
+                console.error("Error al obtener las compatibilidades:", error);
+            });
     }, [userId]);
+
+    // Función para obtener el porcentaje de compatibilidad entre dos usuarios
+    const getCompatibilityPercentage = (user1Id, user2Id) => {
+        const compatibility = compatibilities.find((compat) =>
+            (compat.usuario1_id === user1Id && compat.usuario2_id === user2Id) ||
+            (compat.usuario1_id === user2Id && compat.usuario2_id === user1Id) // Compatibilidad es bidireccional
+        );
+
+        return compatibility ? compatibility.compatibilidad : 'N/A';
+    };
 
     return (
         <section className="py-5">
@@ -42,6 +64,12 @@ const GroupCards = () => {
                                         <div>
                                             <FontAwesomeIcon icon={faBirthdayCake} />{' '}
                                             {new Date(match.birth_date).toLocaleDateString('es-ES', { day: 'numeric', month: 'long' })}
+                                        </div>
+                                        <div>
+                                            {getCompatibilityPercentage(userId, match.user_id)}{' '}
+                                            <span style={{ fontSize: '24px' }}>
+                                                <FaDna />
+                                            </span>
                                         </div>
                                     </div>
                                 </div>
